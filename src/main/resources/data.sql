7fe2eef7f271019d9a2b694a3e789243f151b773
ikatanyi <kennedy.ikatanyi@bringglobal.com>
2024-06-10 11:19:15 +0300

    Oauth Implementation

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -13,5 +13,5 @@ INSERT IGNORE INTO `card-task-db`.auth_user (email,name,password) VALUES
 	 ('user2@user2.com','User2','$2a$10$suErkscQiqeM7Ek1Yv.LJOr9A/qhDhyqZrqqyTkn/ceyq3ag799Vq'),
 	 ('admin@admin.com','ADMIN','$2a$10$suErkscQiqeM7Ek1Yv.LJOr9A/qhDhyqZrqqyTkn/ceyq3ag799Vq');
 INSERT IGNORE INTO `card-task-db`.task_card (color,created_on,description,name,card_status,user_id) VALUES
-	 ('#678578','2024-04-22','description','card-name1',0,10),
-	 ('#678579','2024-04-22','description','card-name2',0,10);
+	 ('#678578','2024-04-22','description','card-name1',0,4),
+	 ('#678579','2024-04-22','description','card-name2',0,4);
