618a298db5a7e04252d82de2a296a7054c779703
ikatanyi <kennedy.ikatanyi@bringglobal.com>
2024-06-11 14:38:51 +0300

    Oauth Implementation

--- src/main/java/com/logicea/card/security/config/OpenApiConfig.java
+++ src/main/java/com/logicea/card/security/config/OpenApiConfig.java
@@ -4,16 +4,37 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.security.SecuritySchemes;
+import jakarta.validation.constraints.Email;
+import jdk.jfr.Name;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@OpenAPIDefinition(info = @Info(title = "Card Task REST API", version = "1.1",
-        contact = @Contact(name = "kennedy ikatanyi", email = "ikatanyi@gmail.com")),
-        security = {@SecurityRequirement(name = "basicAuth"), @SecurityRequirement(name = "bearerToken")}
+@OpenAPIDefinition(
+    info = @Info(
+        title = "CARD TASK REST API",
+        version = "1.0",
+        termsOfService = "#",
+        contact = @Contact(
+            name = "Kennedy Ikatanyi",
+            url = "https://worldnettps.com/contact/",
+            email = "support@worldnettps.com"),
+        license = @License(
+            name = "Use and Distribution License",
+            url = "#"),
+        description = "This is a simple Spring boot application focused on the interaction with a Spring REST server.\n"
+            + "\n"
+            + "The data-centric application offers a basic user-management section, with the possibility to sign-in new users.\n"
+            + "Each user can manage a list of task cards, each one associated to a name, color, status date, and a description.\n"
+            + "There are three different roles:\n"
+            + "* Admin: has full permissions on the whole data\n"
+            + "* Member manager: has permissions on user data\n"
+            + "* User: has full permissions on owned Card data.")
 )
+
 @SecuritySchemes({
         @SecurityScheme(name = "basicAuth", type = SecuritySchemeType.HTTP, scheme = "basic"),
         @SecurityScheme(name = "bearerToken", type = SecuritySchemeType.HTTP, scheme = "bearer", bearerFormat = "JWT")
